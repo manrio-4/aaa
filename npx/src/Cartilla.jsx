ce10f84e1829db4a8056b08708782b7ce43a7e7b
manrio-4 <4.hcovarrubiasmario@gmail.com>
2024-11-21 17:42:14 +0000

    Pending changes exported from your codespace

---
+++ npx/src/Cartilla.jsx
@@ -0,0 +1,38 @@
+import "./estilos.css"
+export default function Cartilla()
+{
+    return(
+        <div className="abuelo">
+            <div className="padre">
+                <div className="tit">titulo</div>
+                <div className="imag">
+                    <img src="https://i0.wp.com/fmbolivia.com.bo/wp-content/uploads/2024/09/Morenada.jpg?resize=750%2C430&ssl=1" alt="" />
+                </div>
+                <div className="desc">
+                 <h2>descripcion</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam consectetur quos magnam inventore in cum, ipsa necessitatibus molestiae nesciunt quibusdam, accusamus a architecto deleniti soluta dolor minima. Ullam, saepe facilis.</p>
+                </div>
+            </div>
+            <div className="padre">
+                <div className="tit">titulo</div>
+                <div className="imag">
+                    <img src="https://i0.wp.com/fmbolivia.com.bo/wp-content/uploads/2024/09/Morenada.jpg?resize=750%2C430&ssl=1" alt="" />
+                </div>
+                <div className="desc">
+                 <h2>descripcion</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam consectetur quos magnam inventore in cum, ipsa necessitatibus molestiae nesciunt quibusdam, accusamus a architecto deleniti soluta dolor minima. Ullam, saepe facilis.</p>
+                </div>
+            </div>
+            <div className="padre">
+                <div className="tit">titulo</div>
+                <div className="imag">
+                    <img src="https://i0.wp.com/fmbolivia.com.bo/wp-content/uploads/2024/09/Morenada.jpg?resize=750%2C430&ssl=1" alt="" />
+                </div>
+                <div className="desc">
+                 <h2>descripcion</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam consectetur quos magnam inventore in cum, ipsa necessitatibus molestiae nesciunt quibusdam, accusamus a architecto deleniti soluta dolor minima. Ullam, saepe facilis.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
